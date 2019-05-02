cfe503af2d58b09dd7679f028147028e6bd00edc
David Floegel <mail@davidfloegel.com>
2019-05-02 08:24:35 +0100

    update paths

--- set-me-up.sh
+++ set-me-up.sh
@@ -2,6 +2,23 @@
 
 CONFIG=$(pwd)
 
-ln -s ${CONFIG}/vim/vimrc ~/.vimrc
-ln -s ${CONFIG}/vim/ultisnips ~/.vim/ultisnips
-ln -s ${CONFIG}/vim/autoload ~/.vim/autoload
+if [ ! -f ~/.vimrc ]; then
+  ln -s ${CONFIG}/vim/vimrc ~/.vimrc
+  echo "✅ Linked vimrc config to ~/.vimrc"
+else
+  echo "🚫 .vimrc file already linked, ignore"
+fi
+
+if [ ! -f ~/.vim/ultisnips ]; then
+  ln -s ${CONFIG}/vim/ultisnips ~/.vim/ultisnips
+  echo "✅ Linked vim ultisnips to ~/.vim/ultisnips"
+else
+  echo "🚫 autoload config already linked, ignore"
+fi
+
+if [ ! -f ~/.vim/autoload ]; then
+  ln -s ${CONFIG}/vim/autoload ~/.vim/autoload
+  echo "✅ Linked vim autoload to ~/.vim/autoload"
+else
+  echo "🚫 autoload config already linked, ignore"
+fi
