f5a8e03fd023d82ef75ce5a271d6d2be39247326
David Floegel <mail@davidfloegel.com>
2023-10-31 14:01:51 +0000

    add neovim folder with lua config

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,45 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+  'lua_ls',
+	'tsserver',
+	'eslint',
+})
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+	sign_icons = { }
+})
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
